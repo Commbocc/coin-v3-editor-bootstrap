c0d785ef7be4c063834065b4ec1a8f8901697990
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-03-29 09:09:01 -0400

    remove coin-icon pseudo color bar

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,17 @@
 <template lang="html">
   <div class="">
-    <h1>Editor Styles</h1>
+    <h1>Bootstrap Demo</h1>
+
+    <h2 class="mt-5">Utilities</h2>
+
+    <div class="mx-auto text-center border border-dark w-50 p-2 mb-3" style="">
+      <p>centered inline element, border, 50% width, padding, margin-bottom</p>
+      <code>mx-auto text-center border border-dark w-50 p-2 mb-3</code>
+    </div>
+
+    <div class="bg-info text-white p-3">
+      <code class="">bg-info text-white p-3</code>
+    </div>
 
     <h2 class="mt-5">Grid</h2>
 
@@ -32,17 +43,6 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
       </div>
-    </div>
-
-    <h2 class="mt-5">Utilities</h2>
-
-    <div class="mx-auto text-center border border-dark w-50 p-2 mb-3" style="">
-      <p>centered inline element, border, 50% width, padding, margin-bottom</p>
-      <code>mx-auto text-center border border-dark w-50 p-2 mb-3</code>
-    </div>
-
-    <div class="bg-info text-white p-3">
-      <code class="">bg-info text-white p-3</code>
     </div>
 
     <h2 class="mt-5">Buttons</h2>
@@ -82,28 +82,28 @@
     <h2 class="mt-5">Alerts</h2>
 
     <div class="alert alert-primary" role="alert">
-      This is a primary alert—check it out!
+      This is a primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-secondary" role="alert">
-      This is a secondary alert—check it out!
+      This is a secondary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-success" role="alert">
-      This is a success alert—check it out!
+      This is a success alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-danger" role="alert">
-      This is a danger alert—check it out!
+      This is a danger alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-warning" role="alert">
-      This is a warning alert—check it out!
+      This is a warning alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-info" role="alert">
-      This is a info alert—check it out!
+      This is a info alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-light" role="alert">
-      This is a light alert—check it out!
+      This is a light alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
     <div class="alert alert-dark" role="alert">
-      This is a dark alert—check it out!
+      This is a dark alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
     </div>
 
   </div>
