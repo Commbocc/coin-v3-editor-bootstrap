3b4987ca69adf5ced0474c449d044d5b92db5399
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-05-21 16:07:47 -0400

    link in test text

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -8,7 +8,7 @@
       <img class="mr-3" src="http://placehold.it/100x100&text=1" alt="Generic placeholder image">
       <div class="media-body">
         <h5 class="mt-0">Media heading</h5>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        Cras sit amet nibh libero, in <a href="#">gravida</a> nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
       </div>
     </div>
 
